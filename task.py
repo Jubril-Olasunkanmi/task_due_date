d4241d6a2bbddeac3adcc823a41cb3ae2d35b161
Jubril-Olasunkanmi <73476744+Jubril-Olasunkanmi@users.noreply.github.com>
2025-05-08 11:29:47 +0100

    Update task.py

--- task.py
+++ task.py
@@ -31,7 +31,7 @@ def append_data(new_entry):
     set_with_dataframe(worksheet, combined_df)
 
 # --- Streamlit App ---
-st.title("📝 Task Tracker with Google Sheets Backend (with Scheduler)")
+st.title("📝 Task Tracker")
 
 df = load_data()
 
